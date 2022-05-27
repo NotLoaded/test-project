e60c289b45f7be528d01c106a400eb4208f0dee3
NotLoaded <eckhardt.cle@gmail.com>
2022-05-27 15:47:55 +0200

    master

---
+++ python testing/test.py
@@ -0,0 +1,9 @@
+name = "Lil Bastard"
+home_location = "California, Las Vegas"
+age = 28
+birthdate = 1994
+telephone_number = "02453 24534"
+
+print("Mein Name ist", name, ".")
+print("Ich bin am", birthdate, " geboren und bin", age, " Jahre alt.")
+print("Ich lebe in", home_location)
