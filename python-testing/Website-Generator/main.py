d2c792eebad3e3a772d92d2a6c8c44b81853ab4d
NotLoaded <eckhardt.cle@gmail.com>
2022-05-31 20:26:19 +0200

    master

--- python-testing/Website-Generator/main.py
+++ python-testing/Website-Generator/main.py
@@ -34,9 +34,59 @@ base_html = """
 </html>
 """
 
-html = base_html
+
+html_b1 = """
+<html>
+    <head>
+        <title> </title>
+    
+       <style>
+          img {
+"""
+
+html_img_bhw = """                width: """
+html_img_bhh = """
+                height: """
+
+html_b2 = """
+                object-fit: cover;
+           }
+        </style>
+    </head>
+    <body>
+        <h1>"""
+
+html_b3 = """</h1>
+    </body>
+
+    <body>
+    """
+
+html_img_sd1 = '''        <img src="'''
+html_img_sd2 = '''">
+'''
+
+
+html_t = "Pictures"
+
+
+img_cw = "400px"
+img_ch = "150px"
+img_1 = "img/1.png"
+img_2 = "img/2.png"
+img_3 = "img/3.png"
+
+html_img = html_img_sd1 + img_1 + html_img_sd2 + html_img_sd1 + img_2 + html_img_sd2 + html_img_sd1 + img_3 + html_img_sd2
+
+
+html = html_b1 + html_img_bhw + img_cw + html_img_bhh + img_ch + html_b2 + html_t + html_b3
+
 
 print(html)
+print()
+print()
+print(html_img)
+
 
 with open(filepath, "w") as file:
-    file.write(html)
+    file.write(base_html)
